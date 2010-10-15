97ec5e04040c8d507daaeffb38b49980fe54030e
Bess Sadler <bess@stanford.edu>
2010-10-15 13:41:38 -0700

    Added the ability to change log levels to LyberCore::Log

--- lib/lyber_core/log.rb
+++ lib/lyber_core/log.rb
@@ -4,8 +4,12 @@ module LyberCore
   class LyberCore::Log
     require 'logger'
         
+    # Default values
+    default_log_level = 3
+        
     @@logfile = "/tmp/lybercore_log.log"
-    @@log ||= Logger.new(@@logfile, 20, 'daily')
+    @@log ||= Logger.new(@@logfile)
+    @@log.level = default_log_level
     # @@log = Logger.new()
     
       # @logfile = logfile ? logfile : "/tmp/lybercore_logger.log"
@@ -19,18 +23,49 @@ module LyberCore
     #   return @@log
     # end
     
+    # Accepts a filename as an argument, and checks to see whether that file can be 
+    # opened for writing. If it can be opened, it closes the existing Logger object
+    # and re-opens it with the new logfile location. It raises an exception if it
+    # cannot write to the specified logfile. 
     def Log.set_logfile(new_logfile)
       
       begin
         File.open(new_logfile, 'w') {}
-        raise "Couldn't open file #{filename} for writing" unless File.writable?(new_logfile) 
-        @@log.close
+        raise "Couldn't open file #{new_logfile} for writing" unless File.writable?(new_logfile) 
         @@logfile = new_logfile
-        @@log ||= Logger.new(@@logfile, 20, 'daily')      
+        @@log ||= Logger.new(@@logfile)      
       rescue Exception => e
-        raise e, "Couldn't initialize logfile: #{e.backtrace}"
+        raise e, "Couldn't initialize logfile #{new_logfile}: #{e.backtrace}"
       end
       
+    end
+    
+    # Set the log level. 
+    # See http://ruby-doc.org/core/classes/Logger.html for more info. 
+    # Possible values are: 
+    #   Logger::FATAL (4):  an unhandleable error that results in a program crash
+    #   Logger::ERROR (3):  a handleable error condition
+    #   Logger::WARN (2): a warning
+    #   Logger::INFO (1): generic (useful) information about system operation
+    #   Logger::DEBUG (0):  low-level information for developers
+    def Log.set_level(loglevel)
+     begin
+        if [0,1,2,3,4].contains? loglevel
+          @@log.level = loglevel
+          @@log.debug "Setting LyberCore::Log.level to #{loglevel}"
+        else
+          @@log.warn "I received an invalid option for log level. I expected a number between 0 and 4 but I got #{loglevel}"
+          @@log.warn "I'm setting the loglevel to 0 (debug) because you seem to be having trouble."
+          @@log.level = 0
+        end
+      rescue Exception => e
+        raise e, "Couldn't set log level: #{e.backtrace}"
+      end
+    end
+    
+    # Return the current log level
+    def Log.level
+      @@log.level
     end
     
     
--- spec/lyber_core/log_spec.rb
+++ spec/lyber_core/log_spec.rb
@@ -20,35 +20,57 @@ describe LyberCore::Log do
       LyberCore::Log.logfile.should eql(default_logfile)
     end
     
-    it "can set the location of the logfile" do
+    it "can set the location of the logfile and then set it back again" do
+      LyberCore::Log.set_logfile(default_logfile)
+      LyberCore::Log.logfile.should eql(default_logfile)
       LyberCore::Log.set_logfile(valid_logfile)
       LyberCore::Log.logfile.should eql(valid_logfile)
+      LyberCore::Log.set_logfile(default_logfile)
+      LyberCore::Log.logfile.should eql(default_logfile)
     end
 
-    it "throws an error if passed an invalid location for a logfile" do
+    it "throws an error and does not change location of logfile if passed an invalid location for a logfile" do
+      LyberCore::Log.set_logfile(default_logfile)
+      LyberCore::Log.logfile.should eql(default_logfile)
       lambda { LyberCore::Log.set_logfile(invalid_logfile)}.should raise_exception(/Couldn't initialize logfile/)         
+      LyberCore::Log.logfile.should eql(default_logfile)
+    end
+    
+    it "can report its log level" do
+      LyberCore::Log.should respond_to(:level)
     end
 
-          # it "starts in error reporting mode (log level 3)" do
-          #   robot = TestRobot.new(wf_name, wf_step, :logfile => valid_logfile)
-          #   robot.log_level.should eql(Logger::ERROR) 
-          # end
-          # 
-          # it "can be put into debug mode (log level 0)" do
-          #   robot = TestRobot.new(wf_name, wf_step, :logfile => valid_logfile)
-          #   robot.set_log_level(Logger::DEBUG)
-          #   robot.log_level.should eql(Logger::DEBUG) 
-          # end
+    it "starts in error reporting mode (log level 3)" do
+      LyberCore::Log.level.should eql(Logger::ERROR)       
+    end
+    
+    it "can be put into debug mode (log level 0)" do
+      LyberCore::Log.set_level(3)
+      LyberCore::Log.level.should eql(Logger::ERROR)       
+      LyberCore::Log.set_level(0)
+      LyberCore::Log.level.should eql(Logger::DEBUG)       
+    end
+    
+    it "goes into debug mode if it receives an invalid option for loglevel" do
+      LyberCore::Log.set_level(3)
+      LyberCore::Log.level.should eql(Logger::ERROR)       
+      LyberCore::Log.set_level("foo")
+      LyberCore::Log.level.should eql(Logger::DEBUG)
+    end
+    
+    # it "keeps the same logging level when switching to a different file" do
+    #   LyberCore::Log.set_level(0)
+    #   LyberCore::Log.set_logfile(default_logfile)
+    #   LyberCore::Log.set_logfile(valid_logfile)
+    #   LyberCore::Log.level.should eql(Logger::DEBUG)       
+    # end
           # 
           # it "can be instantiated in debug mode (log level 0)" do
           #   robot = TestRobot.new(wf_name, wf_step, :logfile => valid_logfile, :loglevel => 0)
           #   robot.log_level.should eql(Logger::DEBUG) 
           # end
           # 
-          # it "goes into debug mode if it receives an invalid option for loglevel" do
-          #   robot = TestRobot.new(wf_name, wf_step, :logfile => valid_logfile, :loglevel => "foo")
-          #   robot.log_level.should eql(Logger::DEBUG) 
-          # end
+
           # 
           # it "can write to the log file" do
           #   onetimefile = "/tmp/onetimefile"
