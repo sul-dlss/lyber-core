c874d9190950963e5ff99772d95dba381c8f3021
Bess Sadler <bess@stanford.edu>
2010-10-15 17:08:49 -0700

    Now that logging is in the LyberCore::Log class, I'm removing those methods from LyberCore::Robots::Robot

--- lib/lyber_core/robots/robot.rb
+++ lib/lyber_core/robots/robot.rb
@@ -7,7 +7,6 @@ module LyberCore
   module Robots
     require 'optparse'
     require 'ostruct'
-    require 'logger'
 
     # ===== Usage
     # User defined robots should derive from this class and override the #process_item method
@@ -21,12 +20,9 @@ module LyberCore
       attr_accessor :workspace
       attr_accessor :args
       attr_accessor :options
-      
-      # Logging accessor methods
-      attr_accessor :logfile
-      attr_accessor :logger
 
-      # ==== Available options
+
+      # Available options
       # - :collection_name - The collection this workflow should work with.  
       #   Defined as a subdirectory within ROBOT_ROOT/config/workflows/your_workflow/your_collection
       # - :workspace - Full path of where to find content for a particular workflow
@@ -35,100 +31,28 @@ module LyberCore
         @workflow_step = workflow_step
         @collection_name = args[:collection_name]      
         @opts = args
+        
+        LyberCore::Log.set_logfile(args[:logfile]) if args[:logfile] 
       
         # Set defaults
         @options = OpenStruct.new
         @options.verbose = false
         @options.quiet = false
         self.parse_options
-        self.start_logging(args)
         self.create_workflow
       end
 
-      # ===== Set up the logging system
-      # ===== Pass in the location of the desired logfile as an argument, like this:
-      #   robot = TestRobot.new(wf_name, wf_step, :logfile => "/path/to/logfile.log")
-      # ===== If the logfile is not passed in during instantiation, it gets a default value of 
-      #   /tmp/logfile.log
-      # ===== Pass in the log level of a robot like this:
-      #   robot = TestRobot.new(wf_name, wf_step, :loglevel => 0)
-      # ===== where loglevel is one of these:
-      #   Logger::FATAL (4):  an unhandleable error that results in a program crash
-      #   Logger::ERROR (3):  a handleable error condition
-      #   Logger::WARN (2): a warning
-      #   Logger::INFO (1): generic (useful) information about system operation
-      #   Logger::DEBUG (0):  low-level information for developers
-      # ===== An invalid option passed to :loglevel will put the robot in debug mode
-      def start_logging(args)          
-          @logfile = "/tmp/logfile.log"             
-          
-          # Check for the presence of args[:logfile] and attempt to open the indicated file
-          begin
-            if args[:logfile]
-              filename = args[:logfile]
-              File.open(filename, 'w') {}
-              raise "Couldn't open file #{filename} for writing" unless File.writable?(filename) 
-              @logfile = filename
-            end
-          rescue Exception => e
-            raise e, "Couldn't initialize logfile: #{e.backtrace}"
-          end
-          
-          # Instantiate the Logger object with the current logfile value
-          @logger = Logger.new(@logfile)
-
-          # Check for the presence of args[:loglevel] and set the log level appropriately
-          begin
-            if args[:loglevel]
-              loglevel = args[:loglevel]
-              if [0,1,2,3,4].contains? loglevel
-                @logger.level = loglevel
-                @logger.debug "Setting @logger.loglevel to #{loglevel}"
-              else
-                @logger.warn "I received an invalid option for log level. I expected a number between 0 and 4 but I got #{loglevel}"
-                @logger.warn "I'm setting the loglevel to 0 (debug) because you seem to be having trouble."
-                @logger.level = 0
-              end
-            else
-              @logger.level = Logger::ERROR
-            end
-          rescue Exception => e
-            raise e, "Couldn't set log level: #{e.backtrace}"
-          end
-
-          @logger.formatter = proc{|s,t,p,m|"%5s [%s] (%s) %s :: %s\n" % [s, 
-                             t.strftime("%Y-%m-%d %H:%M:%S"), $$, p, m]}
-    
-      end
-      
-      # === Set the log level. 
-      # See http://ruby-doc.org/core/classes/Logger.html for more info. 
-      # Possible values are: 
-      #   Logger::FATAL (4):  an unhandleable error that results in a program crash
-      #   Logger::ERROR (3):  a handleable error condition
-      #   Logger::WARN (2): a warning
-      #   Logger::INFO (1): generic (useful) information about system operation
-      #   Logger::DEBUG (0):  low-level information for developers
-      def set_log_level(log_level)
-        @logger.level = log_level
-      end
-      
-      # === Returns the current log level of the robot
-      def log_level
-        @logger.level
-      end
-      
       # Create the workflow at instantiation, not when we start running the robot.
       # That way we can do better error checking and ensure that everything is going
       # to run okay before we actually start things.
       def create_workflow
         unless defined?(WORKFLOW_URI)
-          @logger.fatal "FATAL: WORKFLOW_URI is not defined"
-          @logger.fatal "Usually this is a value like 'http://lyberservices-dev.stanford.edu/workflow'"
-          @logger.fatal "Usually you load it by setting ROBOT_ENVIRONMENT when you invoke your robot"
+          LyberCore::Log.fatal "FATAL: WORKFLOW_URI is not defined"
+          LyberCore::Log.fatal "Usually this is a value like 'http://lyberservices-dev.stanford.edu/workflow'"
+          LyberCore::Log.fatal "Usually you load it by setting ROBOT_ENVIRONMENT when you invoke your robot"
           raise "WORKFLOW_URI is not set! Do you need to set your ROBOT_ENVIRONMENT value?"
         end
-        @logger.debug("About to instatiate a Workflow object: LyberCore::Robots::Workflow.new(#{@workflow_name},#{collection_name}")
+        LyberCore::Log.debug("About to instatiate a Workflow object: LyberCore::Robots::Workflow.new(#{@workflow_name},#{collection_name}")
         @workflow = LyberCore::Robots::Workflow.new(@workflow_name, {:logger => @logger, :collection_name => @collection_name})
         
       end
--- spec/lyber_core/robots/robot_spec.rb
+++ spec/lyber_core/robots/robot_spec.rb
@@ -65,173 +65,98 @@ describe LyberCore::Robots::Robot do
     ROBOT_ROOT = File.expand_path(File.dirname(__FILE__) + "/../../fixtures")
     WORKFLOW_URI = 'http://lyberservices-dev.stanford.edu/workflow'
     
-      it "has a logfile" do
-        robot = TestRobot.new(wf_name, wf_step, :collection_name => collection)
-        robot.logfile.should eql("/tmp/logfile.log")
+      it "can set the location of the log file when it is created" do
+        workflow_logfile = "/tmp/workflow_testing.log"
+        robot = TestRobot.new("sdrIngestWF", "populate-metadata", :logfile => workflow_logfile)
+        LyberCore::Log.logfile.should eql(workflow_logfile)
       end
       
-      it "can set the location of the logfile" do
-        robot = TestRobot.new(wf_name, wf_step, :logfile => valid_logfile)
-        robot.logfile.should eql(valid_logfile)
-      end
-      
-      it "throws an error if passed an invalid location for a logfile" do
-        lambda { robot = TestRobot.new(wf_name, wf_step, :logfile => invalid_logfile) }.should raise_exception(/Couldn't initialize logfile/)        
-      end
-
-      it "starts in error reporting mode (log level 3)" do
-        robot = TestRobot.new(wf_name, wf_step, :logfile => valid_logfile)
-        robot.log_level.should eql(Logger::ERROR) 
-      end
-      
-      it "can be put into debug mode (log level 0)" do
-        robot = TestRobot.new(wf_name, wf_step, :logfile => valid_logfile)
-        robot.set_log_level(Logger::DEBUG)
-        robot.log_level.should eql(Logger::DEBUG) 
-      end
-      
-      it "can be instantiated in debug mode (log level 0)" do
-        robot = TestRobot.new(wf_name, wf_step, :logfile => valid_logfile, :loglevel => 0)
-        robot.log_level.should eql(Logger::DEBUG) 
-      end
-      
-      it "goes into debug mode if it receives an invalid option for loglevel" do
-        robot = TestRobot.new(wf_name, wf_step, :logfile => valid_logfile, :loglevel => "foo")
-        robot.log_level.should eql(Logger::DEBUG) 
-      end
-      
-      it "can write to the log file" do
-        onetimefile = "/tmp/onetimefile"
-        File.delete onetimefile if File.exists? onetimefile
-        robot = TestRobot.new(wf_name, wf_step, :logfile => onetimefile)
-        (File.file? onetimefile).should eql(true)
-        robot.logger.error("This is an error")
-        robot.logger.debug("Debug info 1")
-        robot.logger.info("This is some info")
-        robot.logger.error("This is another error")
-        contents = open(onetimefile) { |f| f.read }
-        contents.should match(/This is an error/)
-        contents.should match(/This is another error/)
-        contents.should_not match(/This is some info/)
-        contents.should_not match(/Debug info 1/)
-        File.delete onetimefile
-        (File.exists? onetimefile).should eql(false)
-        
-      end
-      
-      it "prints debugging statements when in debugging mode" do
-        onetimefile = "/tmp/debugfile"
-        File.delete onetimefile if File.exists? onetimefile
-        robot = TestRobot.new(wf_name, wf_step, :logfile => onetimefile, :loglevel => 0)
-        (File.file? onetimefile).should eql(true)
-        robot.logger.debug("Debug info 1")
-        robot.logger.fatal("Oh nooooo!")
-        robot.logger.debug("More debug stuff")
-        robot.logger.info("And here is some info")
-        contents = open(onetimefile) { |f| f.read }
-        contents.should match(/Debug info 1/)
-        contents.should match(/Oh nooooo!/)
-        File.delete onetimefile
-        (File.exists? onetimefile).should eql(false)
-      end
-      
-      # it "prints debugging statements when in debugging mode" do  
-      #   robot = TestRobot.new("sdrIngestWF", "populate-metadata", :logfile => valid_logfile)
-      #   # puts robot.inspect
-      #   # robot.start
-      # end
-    
-  end
-    
-  context "workflow" do
-    
-    before :each do
-      require File.expand_path(File.dirname(__FILE__) + "/../../fixtures/config/environments/test.rb")  
     end
-    
-    require 'dor_service'      
-    ROBOT_ROOT = File.expand_path(File.dirname(__FILE__) + "/../../fixtures") unless defined? ROBOT_ROOT
-    workflow_logfile = "/tmp/workflow_testing.log"
-    
-    it "can inspect its workflow object" do
-      robot = TestRobot.new("sdrIngestWF", "populate-metadata", :logfile => workflow_logfile)
-      robot.workflow.repository.should eql("sdr")
-    end
-    
-    it "shares a logger object with its workflow object" do
-      robot = TestRobot.new("sdrIngestWF", "populate-metadata", :logfile => workflow_logfile)
-      robot.workflow.logger.should eql(robot.logger)
-    end
-  
-  end
-  
-  context "other" do
-  
-    it "can accept a single druid for processing" do
-      mock_workflow = mock('workflow')
-      mock_queue = mock('queue')
-      ARGV << "--druid=sdrtwo:blah"
-      robot = TestRobot.new('googleScannedBook', 'descriptive-metadata', :collection_name => 'publicDomain')
-      robot.get_druid_list[0].should eql("sdrtwo:blah")
-    end
-  
-    it "can accept a file of druids for processing" do
-      mock_workflow = mock('workflow')
-      mock_queue = mock('queue')
-      ARGV << "--file=fakefile"
-      robot = TestRobot.new('googleScannedBook', 'descriptive-metadata', :collection_name => 'publicDomain')
-      robot.options.file.should eql("fakefile")
-    end
-    
-    # Cucumber passes "--format pretty" as an argument, which can make the robots fail unless
-    # we check for it. 
-    it "shouldn't fail when run by cucumber" do
-      mock_workflow = mock('workflow')
-      mock_queue = mock('queue')
-      ARGV << "--format pretty"
-      lambda { TestRobot.new('googleScannedBook', 'descriptive-metadata', :collection_name => 'publicDomain') }.should_not raise_exception()
-    end  
-  
-    it "should process a batch of druids from the Workflow" do   
       
-      require File.expand_path(File.dirname(__FILE__) + "/../../fixtures/config/environments/test.rb")  
-
-      robot = TestRobot.new("googleScannedBook", "descriptive-metadata", :collection_name => 'publicDomain')
-      puts robot.workflow.inspect
-      puts robot.workflow.queue('descriptive-metadata')
-
-      # mock_workflow = mock('workflow')
-      # mock_queue = mock('queue')
-      # robot = TestRobot.new('googleScannedBook', 'descriptive-metadata', :collection_name => 'publicDomain')
-      # LyberCore::Robots::Workflow.should_receive(:new).and_return(mock_workflow)
-      # mock_workflow.should_receive(:queue).with('descriptive-metadata').and_return(mock_queue)
-      # ARGV.stub!(:size).and_return(0)
-      # mock_queue.should_receive(:enqueue_workstep_waiting)
-      # robot.should_receive(:process_queue).and_return(nil)
-      # robot.start
+    context "workflow" do
+      
+      before :each do
+        require File.expand_path(File.dirname(__FILE__) + "/../../fixtures/config/environments/test.rb")  
+      end
+      
+      require 'dor_service'      
+      ROBOT_ROOT = File.expand_path(File.dirname(__FILE__) + "/../../fixtures") unless defined? ROBOT_ROOT
+      workflow_logfile = "/tmp/workflow_testing.log"
+      
+      it "can inspect its workflow object" do
+        robot = TestRobot.new("sdrIngestWF", "populate-metadata", :logfile => workflow_logfile)
+        robot.workflow.repository.should eql("sdr")
+      end
+    
     end
-
-    it "should process queue of objects" do
-
-      mock_queue = mock('queue')
-      mock_item = mock('item')
-      mock_mdutils = mock('mdutils')
-      mock_dorservice = mock('dorservice')
-      robot = TestRobot.new('googleScannedBook', 'descriptive-metadata', :collection_name => 'publicDomain')
-
-      #Return the mock item the first time, return nil the second time to stop the while loop
-      mock_queue.should_receive(:next_item).and_return(mock_item, nil)
-      mock_queue.should_receive(:print_stats)
-
-      #inside the while loop
-      mock_item.should_receive(:set_success)
-
-      robot.should_receive(:process_item)
-
-      robot.process_queue(mock_queue)
-
-    end
-  end
+            
+            context "other" do
+            
+              it "can accept a single druid for processing" do
+                mock_workflow = mock('workflow')
+                mock_queue = mock('queue')
+                ARGV << "--druid=sdrtwo:blah"
+                robot = TestRobot.new('googleScannedBook', 'descriptive-metadata', :collection_name => 'publicDomain')
+                robot.get_druid_list[0].should eql("sdrtwo:blah")
+              end
+            
+              it "can accept a file of druids for processing" do
+                mock_workflow = mock('workflow')
+                mock_queue = mock('queue')
+                ARGV << "--file=fakefile"
+                robot = TestRobot.new('googleScannedBook', 'descriptive-metadata', :collection_name => 'publicDomain')
+                robot.options.file.should eql("fakefile")
+              end
+              
+              # Cucumber passes "--format pretty" as an argument, which can make the robots fail unless
+              # we check for it. 
+              it "shouldn't fail when run by cucumber" do
+                mock_workflow = mock('workflow')
+                mock_queue = mock('queue')
+                ARGV << "--format pretty"
+                lambda { TestRobot.new('googleScannedBook', 'descriptive-metadata', :collection_name => 'publicDomain') }.should_not raise_exception()
+              end  
+            
+              it "should process a batch of druids from the Workflow" do   
+                
+                require File.expand_path(File.dirname(__FILE__) + "/../../fixtures/config/environments/test.rb")  
+            
+                robot = TestRobot.new("googleScannedBook", "descriptive-metadata", :collection_name => 'publicDomain')
+                puts robot.workflow.inspect
+                puts robot.workflow.queue('descriptive-metadata')
+            
+                # mock_workflow = mock('workflow')
+                # mock_queue = mock('queue')
+                # robot = TestRobot.new('googleScannedBook', 'descriptive-metadata', :collection_name => 'publicDomain')
+                # LyberCore::Robots::Workflow.should_receive(:new).and_return(mock_workflow)
+                # mock_workflow.should_receive(:queue).with('descriptive-metadata').and_return(mock_queue)
+                # ARGV.stub!(:size).and_return(0)
+                # mock_queue.should_receive(:enqueue_workstep_waiting)
+                # robot.should_receive(:process_queue).and_return(nil)
+                # robot.start
+              end
+            
+              it "should process queue of objects" do
+            
+                mock_queue = mock('queue')
+                mock_item = mock('item')
+                mock_mdutils = mock('mdutils')
+                mock_dorservice = mock('dorservice')
+                robot = TestRobot.new('googleScannedBook', 'descriptive-metadata', :collection_name => 'publicDomain')
+            
+                #Return the mock item the first time, return nil the second time to stop the while loop
+                mock_queue.should_receive(:next_item).and_return(mock_item, nil)
+                mock_queue.should_receive(:print_stats)
+            
+                #inside the while loop
+                mock_item.should_receive(:set_success)
+            
+                robot.should_receive(:process_item)
+            
+                robot.process_queue(mock_queue)
+            
+              end
+      end
   
   
 end
