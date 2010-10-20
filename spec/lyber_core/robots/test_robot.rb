05c7ee9090a262a8ec18247f115a9870af4e47c7
Bess Sadler <bess@stanford.edu>
2010-10-20 16:16:33 -0700

    TestRobot should use central logging too

--- spec/lyber_core/robots/test_robot.rb
+++ spec/lyber_core/robots/test_robot.rb
@@ -2,6 +2,6 @@ require 'lyber_core'
 
 class TestRobot < LyberCore::Robots::Robot
   def process_item(work_item)
-    @logger.debug("Start time is : #{Time.new}")
+    LyberCore::Log.debug("TestRobot: Start time is : #{Time.new}")
   end
 end
