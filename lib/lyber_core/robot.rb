0c614c333b2986e2deeef6d1476ca5a89415507b
Darren Hardy <drh@stanford.edu>
2014-07-22 11:15:10 -0700

    fixes #1

--- lib/lyber_core/robot.rb
+++ lib/lyber_core/robot.rb
@@ -46,7 +46,7 @@ module LyberCore
       #   otherwise, the robot did something like set the step to 'waiting' with a note
 
       Dor::WorkflowService.update_workflow_status @repo, druid, @workflow_name, @step_name, 'completed', :elapsed => elapsed, :note => Socket.gethostname
-      LyberCore::Log.info "Finished #{druid} in #{elapsed}s"
+      LyberCore::Log.info "Finished #{druid} in #{sprintf("%0.4f",elapsed)}s"
     rescue => e
       LyberCore::Log.error e.message + "\n" + e.backtrace.join("\n")
       Dor::WorkflowService.update_workflow_error_status @repo, druid , @workflow_name, @step_name, e.message, :error_text => Socket.gethostname
