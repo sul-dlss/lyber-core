dfb478fcb4c0106fa9657df9981d5680feee128c
Darren Hardy <drh@stanford.edu>
2014-07-31 15:08:04 -0700

    relocate step_to_classname as module method

--- lib/lyber_core/robot.rb
+++ lib/lyber_core/robot.rb
@@ -19,29 +19,30 @@ module LyberCore
         bot = klazz.new
         bot.work druid
       end
-      
-      # Converts a given step to the Robot class name
-      # Examples:
-      #
-      # - `dor:assemblyWF:jp2-create` into `Robots::DorRepo::Assembly::Jp2Create`
-      # - `dor:gisAssemblyWF:start-assembly-workflow` into `Robots::DorRepo::GisAssembly::StartAssemblyWorkflow`
-      # - `dor:etdSubmitWF:binder-transfer` into `Robots:DorRepo::EtdSubmit::BinderTransfer`
-      #
-      # @param [String] step. fully qualified step name, e.g., `dor:accessionWF:descriptive-metadata`
-      # @param [Hash] opts
-      # @option :repo_suffix defaults to `Repo`
-      # @return [String] The class name for the robot, e.g., `Robots::DorRepo::Accession:DescriptiveMetadata`
-      def step_to_classname step, opts = {}
-        # generate the robot job class name
-        opts[:repo_suffix] ||= 'Repo'
-        r, w, s = step.split(/:/, 3)
-        return [
-          'Robots',
-          r.camelcase + opts[:repo_suffix], # 'Dor' conflicts with dor-services
-          w.sub('WF', '').camelcase,
-          s.gsub('-', '_').camelcase
-        ].join('::')
-      end
+    end
+
+    
+    # Converts a given step to the Robot class name
+    # Examples:
+    #
+    # - `dor:assemblyWF:jp2-create` into `Robots::DorRepo::Assembly::Jp2Create`
+    # - `dor:gisAssemblyWF:start-assembly-workflow` into `Robots::DorRepo::GisAssembly::StartAssemblyWorkflow`
+    # - `dor:etdSubmitWF:binder-transfer` into `Robots:DorRepo::EtdSubmit::BinderTransfer`
+    #
+    # @param [String] step. fully qualified step name, e.g., `dor:accessionWF:descriptive-metadata`
+    # @param [Hash] opts
+    # @option :repo_suffix defaults to `Repo`
+    # @return [String] The class name for the robot, e.g., `Robots::DorRepo::Accession:DescriptiveMetadata`
+    def self.step_to_classname step, opts = {}
+      # generate the robot job class name
+      opts[:repo_suffix] ||= 'Repo'
+      r, w, s = step.split(/:/, 3)
+      return [
+        'Robots',
+        r.camelcase + opts[:repo_suffix], # 'Dor' conflicts with dor-services
+        w.sub('WF', '').camelcase,
+        s.gsub('-', '_').camelcase
+      ].join('::')
     end
 
     attr_accessor :check_queued_status
