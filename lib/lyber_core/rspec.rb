886d0e1c4f71848748117bb7ae605bd9851d18fa
Justin Littman <justinlittman@stanford.edu>
2026-07-13 16:51:03 -0400

    Allow providing version for test_perform.

--- lib/lyber_core/rspec.rb
+++ lib/lyber_core/rspec.rb
@@ -3,8 +3,9 @@
 module LyberCore
   # Methods to support testing robots
   module Rspec
-    def test_perform(robot, druid)
+    def test_perform(robot, druid, version: nil)
       allow(robot).to receive(:druid).and_return(druid)
+      allow(robot).to receive(:version).and_return(version) if version
       robot.perform_work
     end
   end
